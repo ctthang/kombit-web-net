6fa50ad461e324ed351da56a2597fa0bd399b05c
nvb@globeteam.com <nvb@globeteam.com>
2016-06-10 10:13:03 +0700

    added provisioning data

---
+++ provisioning/Job role delegate at ang tenant.js
@@ -0,0 +1,30 @@
+//PATH: https://adgangsstyring.eksterntest-stoettesystemerne.dk/admin/api/kmodel/Jobfunktionsroller
+{
+  "Id": "http://adgangsstyring.eksterntest-stoettesystemerne.dk/roles/jobrole/98765432delegated1",
+  "Version": "Version 1.0",
+  "Aktiveringstidspunkt": {
+    "Sluttidspunkt": "2025-03-30 00:00:00",
+    "Starttidspunkt": "2014-01-30 00:00:00"
+  },
+  "DelegeretTil": [
+	{  "Aktiveringstidspunkt": {
+        "Sluttidspunkt": "2019-03-30 00:00:00",
+        "Starttidspunkt": "2016-01-30 00:00:00"
+      },
+      "Cvr": "98765432"
+	},
+	{  "Aktiveringstidspunkt": {
+        "Sluttidspunkt": "2019-03-30 00:00:00",
+        "Starttidspunkt": "2014-01-30 00:00:00"
+      },
+      "Cvr": "20000001"
+	} 	
+  ],
+  "Anvenderkontekst": {
+    "Aktiveringstidspunkt": {
+      "Sluttidspunkt": "2018-03-30 00:00:00",
+      "Starttidspunkt": "2013-01-30 00:00:00"
+    },
+    "Cvr": "20000000"
+  }
+}
---
+++ provisioning/User role defined at ang tenant.js
@@ -0,0 +1,19 @@
+//PATH: https://adgangsstyring.eksterntest-stoettesystemerne.dk/admin/api/kmodel/Brugersystemroller
+{
+	"Id" : "http://adgangsstyring.eksterntest-stoettesystemerne.dk/roles/usersystemrole/98765432defined",
+	"Version" : "1.0",
+	"Aktiveringstidspunkt" : {
+		"Sluttidspunkt" : "2018-04-10T19:59:59Z",
+		"Starttidspunkt" : "2015-03-09T22:00:00Z"
+	},
+	"Dataafgraensningstyper" : [{
+			"Id" : "http://adgangsstyring.eksterntest-stoettesystemerne.dk/constraints/98765432defined",
+			"SyntaxDefinition" : "Syn 1",
+			"PresentationPreference" : "Test 3",
+			"ValidationService" : "http://adgangsstyring.projekt-stoettesystemerne.dk",
+			"PresentationName" : "Name",
+			"Description" : "Defined user role",
+			"Values" : ["defined 1", "defined 2"]
+		}
+	]
+}
